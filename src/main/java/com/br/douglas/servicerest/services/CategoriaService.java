f4142a93ea3696b93e41ba67e4a2be5d7e7e2c26
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-04-23 21:23:14 -0300

    Atualizando categoria com PUT e outros ajustes

--- src/main/java/com/br/douglas/servicerest/services/CategoriaService.java
+++ src/main/java/com/br/douglas/servicerest/services/CategoriaService.java
@@ -24,4 +24,9 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		return repo.saveAndFlush(obj);
 	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.saveAndFlush(obj);
+	}
 }
