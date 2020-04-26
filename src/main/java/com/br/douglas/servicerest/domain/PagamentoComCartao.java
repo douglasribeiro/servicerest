6748db6a26e22aa75a306f8409a6a31aba280c2f
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-04-26 15:51:03 -0300

    Criando profile de teste

--- src/main/java/com/br/douglas/servicerest/domain/PagamentoComCartao.java
+++ src/main/java/com/br/douglas/servicerest/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.br.douglas.servicerest.domain;
 import javax.persistence.Entity;
 
 import com.br.douglas.servicerest.domain.enus.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/br/douglas/servicerest/services/PedidoService.java
+++ src/main/java/com/br/douglas/servicerest/services/PedidoService.java
@@ -1,23 +1,63 @@
 package com.br.douglas.servicerest.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.br.douglas.servicerest.domain.ItemPedido;
+import com.br.douglas.servicerest.domain.PagamentoComBoleto;
 import com.br.douglas.servicerest.domain.Pedido;
+import com.br.douglas.servicerest.domain.enus.EstadoPagamento;
+import com.br.douglas.servicerest.repositories.ItemPedidoRepository;
+import com.br.douglas.servicerest.repositories.PagamentoRepository;
 import com.br.douglas.servicerest.repositories.PedidoRepository;
 import com.br.douglas.servicerest.services.exceptions.ObjectNotFoundException;
 
+
+
 @Service
 public class PedidoService {
-
+	
 	@Autowired
 	private PedidoRepository repo;
-
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
